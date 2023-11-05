b558dfacc8e38616ec8bd10a953dee498d32b5c2
Pelayo <phuermij@myuax.com>
2023-11-05 12:50:48 +0100

    cambios

--- Builder/Builder.py
+++ Builder/Builder.py
@@ -36,7 +36,220 @@ class MasaFermentada(Masa):
 
     def tipo_masa(self) -> str:
         return "Masa Fermentada"
+    
+class Base(ABC):
+    """
+    La interfaz Base declara operaciones para todos los tipos de objetos Base.
+    """
 
+    @abstractmethod
+    def tipo_base(self) -> str:
+        pass
+
+class BaseTomate(Base):
+    """
+    Las clases concretas de Base implementan la interfaz Base.
+    """
+
+    def tipo_base(self) -> str:
+        return "Base de Tomate"
+
+class BaseVegana(Base):
+    """
+    Las clases concretas de Base implementan la interfaz Base.
+    """
+
+    def tipo_base(self) -> str:
+        return "Base Vegana"
+
+class BaseEspecial(Base):
+    """
+    Las clases concretas de Base implementan la interfaz Base.
+    """
+
+    def tipo_base(self) -> str:
+        return "Base Especial"
+
+class Ingredientes(ABC):
+    """
+    La interfaz Ingredientes declara operaciones para todos los tipos de objetos Ingredientes.
+    """
+
+    @abstractmethod
+    def tipo_ingredientes(self) -> str:
+        pass
+
+class IngredienteQueso(Ingredientes):
+    """
+    Las clases concretas de Ingredientes implementan la interfaz Ingredientes.
+    """
+
+    def tipo_ingredientes(self) -> str:
+        return "Queso"
+
+class IngredienteJamon(Ingredientes):
+    """
+    Las clases concretas de Ingredientes implementan la interfaz Ingredientes.
+    """
+
+    def tipo_ingredientes(self) -> str:
+        return "Jamón"
+
+class IngredientePepperoni(Ingredientes):
+    """
+    Las clases concretas de Ingredientes implementan la interfaz Ingredientes.
+    """
+
+    def tipo_ingredientes(self) -> str:
+        return "Pepperoni"
+
+class IngredienteChampiñones(Ingredientes):
+    """
+    Las clases concretas de Ingredientes implementan la interfaz Ingredientes.
+    """
+
+    def tipo_ingredientes(self) -> str:
+        return "Champiñones"
+
+class IngredientePina(Ingredientes):
+    """
+    Las clases concretas de Ingredientes implementan la interfaz Ingredientes.
+    """
+
+    def tipo_ingredientes(self) -> str:
+        return "Piña"
+
+class Coccion(ABC):
+    """
+    La interfaz Coccion declara operaciones para todos los tipos de objetos Coccion.
+    """
+
+    @abstractmethod
+    def tipo_coccion(self) -> str:
+        pass
+
+class CoccionHorno(Coccion):
+    """
+    Las clases concretas de Coccion implementan la interfaz Coccion.
+    """
+
+    def tipo_coccion(self) -> str:
+        return "Horno"
+
+class CoccionLeña(Coccion):
+    """
+    Las clases concretas de Coccion implementan la interfaz Coccion.
+    """
+
+    def tipo_coccion(self) -> str:
+        return "Leña"
+
+class CoccionPiedra(Coccion):
+    """
+    Las clases concretas de Coccion implementan la interfaz Coccion.
+    """
+
+    def tipo_coccion(self) -> str:
+        return "Piedra"
+
+class Presentacion(ABC):
+    """
+    La interfaz Presentacion declara operaciones para todos los tipos de objetos Presentacion.
+    """
+
+    @abstractmethod
+    def tipo_presentacion(self) -> str:
+        pass
+
+class PresentacionCaja(Presentacion):
+    """
+    Las clases concretas de Presentacion implementan la interfaz Presentacion.
+    """
+
+    def tipo_presentacion(self) -> str:
+        return "Caja"
+
+class PresentacionBandeja(Presentacion):
+    """
+    Las clases concretas de Presentacion implementan la interfaz Presentacion.
+    """
+
+    def tipo_presentacion(self) -> str:
+        return "Bandeja"
+
+class PresentacionTroceada(Presentacion):
+    """
+    Las clases concretas de Presentacion implementan la interfaz Presentacion.
+    """
+
+    def tipo_presentacion(self) -> str:
+        return "Troceada"
+
+class Maridajes(ABC):
+    """
+    La interfaz Maridajes declara operaciones para todos los tipos de objetos Maridajes.
+    """
+
+    @abstractmethod
+    def tipo_maridajes(self) -> str:
+        pass
+
+class MaridajeVino(Maridajes):
+    """
+    Las clases concretas de Maridajes implementan la interfaz Maridajes.
+    """
+
+    def tipo_maridajes(self) -> str:
+        return "Vino"
+
+class MaridajeCerveza(Maridajes):
+    """
+    Las clases concretas de Maridajes implementan la interfaz Maridajes.
+    """
+
+    def tipo_maridajes(self) -> str:
+        return "Cerveza"
+
+class MaridajeCoctel(Maridajes):
+    """
+    Las clases concretas de Maridajes implementan la interfaz Maridajes.
+    """
+
+    def tipo_maridajes(self) -> str:
+        return "Coctel"
+
+class Extras(ABC):
+    """
+    La interfaz Extras declara operaciones para todos los tipos de objetos Extras.
+    """
+
+    @abstractmethod
+    def tipo_extras(self) -> str:
+        pass
+
+class ExtraBordeQueso(Extras):
+    """
+    Las clases concretas de Extras implementan la interfaz Extras.
+    """
+
+    def tipo_extras(self) -> str:
+        return "Borde de Queso"
+
+class ExtraTrufa(Extras):
+    """
+    Las clases concretas de Extras implementan la interfaz Extras.
+    """
+
+    def tipo_extras(self) -> str:
+        return "Trufa"
+
+class ExtraCaviar(Extras):
+    """
+    Las clases concretas de Extras implementan la interfaz Extras.
+    """
+
+    def tipo_extras(self) -> str:
+        return "Caviar"
 
 class Builder(ABC):
 
@@ -97,23 +310,23 @@ class PizzaBuilder(Builder):
     def masa(self, masa: Masa) -> None:
         self._pizza.add(masa.tipo_masa())
     
-    def base(self) -> None:
-        self._pizza.add("Base")
+    def base(self, base: Base) -> None:
+        self._pizza.add(base.tipo_base())
     
-    def ingredientes(self) -> None:
-        self._pizza.add("Ingredientes")
+    def ingredientes(self, ingredientes: Ingredientes) -> None:
+        self._pizza.add(ingredientes.tipo_ingredientes())
     
-    def coccion(self) -> None:
-        self._pizza.add("Cocción")
+    def coccion(self, coccion: Coccion) -> None:
+        self._pizza.add(coccion.tipo_coccion())
     
-    def presentacion(self) -> None:
-        self._pizza.add("Presentación")
+    def presentacion(self, presentacion: Presentacion) -> None:
+        self._pizza.add(presentacion.tipo_presentacion())
 
-    def maridajes(self) -> None:
-        self._pizza.add("Maridajes")
+    def maridajes(self, maridajes: Maridajes) -> None:
+        self._pizza.add(maridajes.tipo_maridajes())
 
-    def extras(self) -> None:
-        self._pizza.add("Extras")
+    def extras(self, extras: Extras) -> None:
+        self._pizza.add(extras.tipo_extras())
 
 class Pizza():
     """
